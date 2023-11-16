9c264dd633843cdc706d8b71138d5fbfd1092c8c
Justine Leleu <justine.leleu.28@gmail.com>
2023-11-16 12:14:26 +0100

    start

--- php/index.php
+++ php/index.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pokedex</title>
-    <link rel="stylesheet" href="../css/style.css">
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+
+include("partials/header.php");
+include("partials/footer.php");
+
+?>
---
+++ php/partials/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pokedex</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+    <header class="header">
+        <img src="../../assets/img/pokemon-logo.png" alt="Pokemon logo" class="logo">
+        <nav class="nav">
+            <a href="">Pokemon</a>
+            <a href="">My account</a>
+            <a href="">Register</a>
+        </nav>
+    </header>
