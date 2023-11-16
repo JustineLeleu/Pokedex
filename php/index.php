824de0f0ae447c73d69e362d4e86d85753820f22
Carole Gérard <gerard.carole78@gmail.com>
2023-11-16 12:17:52 +0100

    commit to recover the icons

--- php/index.php
+++ php/index.php
@@ -1,12 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pokedex</title>
-    <link rel="stylesheet" href="../css/style.css">
-</head>
-<body>
-    
-</body>
-</html>
+
+
+<?php 
+$title = "Home";
+require_once __DIR__.'/partials/header.php';
+?>
+
+<main>
+    <h1>Pokedex - Homepage</h1>
+    <p>Hello  <strong>Pikachu</strong></p>
+    <a href="/pokemon?name=pikachu">Pikachu</a>
+</main>
+
+<?php 
+require_once __DIR__.'/partials/footer.php';
+?>
---
+++ php/partials/card.php
@@ -0,0 +1,21 @@
+<link rel="stylesheet" href="../../css/style.css">
+
+
+
+<div class="card-container">
+    <img src="/assets/img/pokemon/bulbasaur.png" alt="Image de Bulbasaur">
+    <p>#0001</p>
+    <h2>
+        Bulbasaur
+    </h2>
+    <div class="card-type">
+        <div>
+            <img src="" alt="">
+            <p>Grass</p>
+        </div>
+        <div>
+            <img src="" alt="">
+            <p>Poison</p>
+        </div>
+    </div>
+</div>
