b995aac08ddbb419fb1b5cefab5da0b8988329c4
tim <timdesmet00@gmail.com>
2023-11-20 09:40:41 +0100

    après merge rosa

--- php/register.php
+++ php/register.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
     require_once __DIR__.'./partials/header.php';
 
@@ -100,7 +99,7 @@ function validateAndSanitizeRegister(&$pseudo, &$email, &$password, &$passwordve
 
 function dataInjection($email, $password, $pseudo) {
     global $pdo;
-    echo "  test fonction dataInjection   ";
+    // echo "  test fonction dataInjection   ";
     try{
     //$sqlreq1 = $pdo->query("SELECT pseudo FROM pokedexuser WHERE pseudo = $pseudo")->fetch();
 
@@ -109,7 +108,7 @@ function dataInjection($email, $password, $pseudo) {
     $sqlreq1 = $pdo->query($query)->fetch();
     
     //$arr = $sqlreq1->execute();
-    echo "<script>console.log('$sqlreq1')</script>";
+    // echo "<script>console.log('$sqlreq1')</script>";
 
     if ($sqlreq1 != null) {
         echo "This email is already used";
@@ -139,46 +138,3 @@ if(array_key_exists('submit', $_POST)) {
 
 
 ?>
-=======
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title><?php echo $title;?> - Pokedex</title>
-        <link href="/scss/style.css" type="text/css" rel="stylesheet">
-        <style>
-            @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;600;700;900&display=swap');
-        </style>
-    </head>
-    <body>
-        <?php
-            include './partials/header.php';
-        ?>
-        <main>
-            <div class="register">
-                <h2>Register</h2>
-                <form method="GET">
-                    <label class="reg" for="email">Email*</label>
-                    <br>
-                    <input type="text" class="reginfo" name="email" id="email">
-                    <br>
-                    <label class="reg" for="password">Password*</label>
-                    <br>
-                    <input type="text" class="reginfo" name="password" id="password">
-                    <br>
-                    <label class="reg" for="passwordverif">Password Verification*</label>
-                    <br>
-                    <input type="text" class="reginfo" name="passwordverif" id="passwordverif">
-                    <br>
-                    <input class="submit" type="submit" name="submit" value="Connexion">
-                </form>
-            </div>
-        </main>
-        <footer>
-            <?php
-                include './partials/footer.php';
-            ?>
-        </footer>
-    </body>
-</html>
->>>>>>> rosa
