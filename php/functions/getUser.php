9ce6fe8600bdde383f198dc8456b6d5e5ba5765b
Justine Leleu <justine.leleu.28@gmail.com>
2023-11-24 06:27:08 +0100

    add button new pokemon for admin only

--- php/functions/getUser.php
+++ php/functions/getUser.php
@@ -5,7 +5,7 @@ function getUser($id)
     $bdd = getBdd();
     $query = "SELECT * FROM pokedexuser WHERE iduser = $id";
     $stmt = $bdd->query($query);
-    $pokemon = $stmt->fetch(PDO::FETCH_ASSOC);
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
     return $user;
 }
 
--- php/index.php
+++ php/index.php
@@ -1,6 +1,7 @@
 <?php
 // require_once __DIR__.'/functions/connectDB.php';
 // $bdd = connectDB();
+session_start();
 
 require __DIR__.'/functions/getBdd.php';
 require __DIR__.'/functions/idPokemonFormatted.php';
@@ -8,6 +9,7 @@ require __DIR__.'/functions/getTypes.php';
 require_once __DIR__.'/functions/getPokemons.php';
 require_once __DIR__.'/functions/getFilterPokemons.php';
 require __DIR__.'/functions/isFavorite.php';
+require __DIR__.'/functions/getUser.php';
 
 if (isset($_POST['search']))
 {
@@ -18,6 +20,15 @@ else
 {
     $arrAllPokemons = getPokemons();
     echo "<script>console.log('no search')</script>";
+}
+
+$isAdmin = false;
+
+if (isset($_SESSION['user']))
+{
+    echo "<script>console.log('is login')</script>";
+    $user = getUser($_SESSION['user']);
+    if ($user['admin']) $isAdmin = true;
 }
 
 ?>
@@ -34,6 +45,7 @@ require_once __DIR__.'/partials/header.php';
         <input type="text" placeholder="Pokemon" name="search" class="searchInput">
         <input type="submit" value="Search" class="searchButton">
     </form>
+    <button onclick="location.href='newPokemon.php';" <?php echo $isAdmin ? '' : 'hidden' ; ?>>Add Pokemon</button>
     <div class="pokemon-grid">
         <?php
             require __DIR__.'/partials/card.php';
